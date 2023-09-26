3fd050381676e22c3b207eaa33f5d1c800249a59
Ezequiel Etchecoin <ezequiel.etchecoin@incluit.com>
2023-09-25 21:57:11 -0300

    moved aws region config to appsettings

--- src/SmartSpend/Server/Configuration/DataAccessSetup.cs
+++ src/SmartSpend/Server/Configuration/DataAccessSetup.cs
@@ -7,13 +7,15 @@ namespace SmartSpend.Server.Configuration
 {
     public static class DataAccessSetup
     {
-        public static void ConfigureDataAccess(this IServiceCollection services)
+        public static void ConfigureDataAccess(this IServiceCollection services, IConfiguration configuration)
         {
             var publicKey = Environment.GetEnvironmentVariable("SMARTSPEND_DATAACCESS_PUBLICKEY");
             var privateKey = Environment.GetEnvironmentVariable("SMARTSPEND_DATAACCESS_PRIVATEKEY");
 
+            var region = Amazon.RegionEndpoint.GetBySystemName(configuration["AWSRegion"]);
+
             services.AddSingleton(sp =>
-                new AmazonDynamoDBClient(publicKey, privateKey, Amazon.RegionEndpoint.USEast1));
+                new AmazonDynamoDBClient(publicKey, privateKey, region));
 
             services.AddSingleton<SmartSpendDbContext>();
 
